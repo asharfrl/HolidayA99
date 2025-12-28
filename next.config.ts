9f55357b161d0d89f9916604654db62911007589
ShZar <asharfarial5@gmail.com>
2025-12-29 02:24:27 +0700

    fix InputField to support controlled input

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    domains: ["firebasestorage.googleapis.com"],
-  },
-
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
       use: ["@svgr/webpack"],
     });
     return config;

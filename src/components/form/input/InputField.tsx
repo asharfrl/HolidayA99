901214e6e1c23a7f1d49574acc171bc9e7e245ff
ShZar <asharfarial5@gmail.com>
2025-12-29 02:38:47 +0700

    fix InputField used by SignInForm (support value & onChange)

--- src/components/form/input/InputField.tsx
+++ src/components/form/input/InputField.tsx
@@ -18,20 +18,15 @@ const InputField = React.forwardRef<HTMLInputElement, InputProps>(
 
         <input
           ref={ref}
-          className={`w-full rounded-lg border px-4 py-2 text-sm outline-none focus:ring-2 focus:ring-blue-500 disabled:opacity-60 ${className}`}
           {...props}
+          className={`w-full rounded-lg border px-4 py-2 text-sm outline-none focus:ring-2 focus:ring-blue-500 disabled:opacity-60 ${className}`}
         />
 
-        {error && (
-          <p className="text-xs text-red-500">
-            {error}
-          </p>
-        )}
+        {error && <p className="text-xs text-red-500">{error}</p>}
       </div>
     );
   }
 );
 
 InputField.displayName = "InputField";
-
 export default InputField;
